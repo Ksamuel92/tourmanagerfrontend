299379e6ac4805999be27a7121b7410731cc5a6b
Kyle Samuel <ksamuel92@gmail.com>
2021-10-03 11:07:34 -0400

    add getShows method to ShowService

--- src/show_service.js
+++ src/show_service.js
@@ -2,4 +2,10 @@ class ShowService {
   constructor(port) {
     this.port = port
   }
+
+  async getShows() {
+    const shows = await fetch(`${this.port}/shows`)
+    .then(shows => shows.json())
+    .then(json => console.log(json))
+  }
 }
