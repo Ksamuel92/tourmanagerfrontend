c05b384b24b272cc7c7068df0b6cb4318a076011
Kyle Samuel <ksamuel92@gmail.com>
2021-10-04 11:27:27 -0400

    declare showGross variable on index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const port = 'http://localhost:3000';
 const showCall = new ShowService(port);
 const showContainer = document.querySelector('#show-container');
+const showGross = document.querySelector('#shows-gross')
 
 showCall.getShows()
 
