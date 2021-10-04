b66f165ec26d37979cd14b74f262b11ae6cf6c27
Kyle Samuel <ksamuel92@gmail.com>
2021-10-04 12:16:20 -0400

    declare value variables for new show form

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const port = 'http://localhost:3000';
 const showCall = new ShowService(port);
 const showContainer = document.querySelector('#show-container');
-const showGross = document.querySelector('#shows-gross')
+const showGross = document.querySelector('#shows-gross');
+const newShowForm = document.querySelector('#new-show-form')
+const venueValue = document.querySelector('#show-venue')
+const cityValue = document.querySelector('#show-city')
+const promoterValue = document.querySelector('#show-promoter')
+const emailValue = document.querySelector('#show-email')
+const loadinValue = document.querySelector('#show-loadin')
+const merchValue = document.querySelector('#show-merch')
+const guaranteeValue = document.querySelector('#show-guarantee')
+const advancedValue = document.querySelector('#show-advanced')
+
 
 showCall.getShows()
+newShowForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault()
+}
 
