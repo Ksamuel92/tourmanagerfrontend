90547a5b3e482ab3a1c2c6bcdf90fd1fd9e89c04
Kyle Samuel <ksamuel92@gmail.com>
2021-10-05 11:14:20 -0400

    build out User class

--- src/user.js
+++ src/user.js
@@ -1,6 +1,25 @@
 class User {
-  constructor({name, email}){
+  static all = []
+
+  constructor({name, email, id}){
     this.name = name
     this.email = email
+    this.element = document.createElement('div');
+    this.element.dataset["id"] = id
+    this.element.className = "user-card"
+    this.element.id = `user-${this.id}`
+    User.all.push(this)
+  }
+
+  render() {
+    this.element.innerHTML = `
+    <h3>Hello ${this.name}!</h3>
+    <p>Get started by creating your first show below!</p>
+    `
+    return this.element
+  }
+
+  attachToDom() {
+    document.body.appendChild(this.element)
   }
 }
