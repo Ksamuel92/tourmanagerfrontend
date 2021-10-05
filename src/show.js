37ad4b48dfe2d3000f8e74ae3bd70bc7b1c27578
Kyle Samuel <ksamuel92@gmail.com>
2021-10-05 16:23:47 -0400

    add renderEditForm() method to Show

--- src/show.js
+++ src/show.js
@@ -15,26 +15,76 @@ constructor({advanced, email, guarantee, id, loadin, merch, promoter, user, venu
   this.element.dataset['id'] = id
   this.element.className = "show-card"
   this.element.id = `show-${this.id}`
+  this.element.addEventListener('click', this.handleClick)
   Show.all.push(this) 
   // debugger
 }
 
 render() {
-  this.element.innerHTML = `
+  this.element.innerHTML = `<div class="show-info">
   <h3>${this.venue} - ${this.city}</h3>
+  <h4> Load In Time: ${new Date(this.loadin)} </h4>
   <ul>
   <li>Promoter: ${this.promoter} - ${this.email}</li>
-  <li>Load In Time: ${new Date(this.loadin)}</li>
   <li>Show Advanced: ${this.advanced ? 'yes' : 'no'}</li>
+  </ul>
   <ul id= "show-${this.id}-gross">
     <li>Merch Total: $${this.merch}</li>
     <li>Show Guarantee: $${this.guarantee}</li>
     <li class="show-totals">Total: $${this.merch + this.guarantee}</li>
   </ul>
-  </ul>
-  <p>This show belongs to ${this.user.email}. </p>
+  </div>
+  <button type="button" data-id:"${this.id}"}">Edit</button>
+  <button type="button" data-id:"${this.id}">Delete</button>
   `
   return this.element
+}
+
+handleClick = e => {
+  if (e.target.innerText === 'Edit'){
+    this.renderEditForm();
+  } else if (e.target.innerText === 'Delete'){
+  } else if (e.target.innerText === 'Save Comment') {
+
+  }
+}
+
+renderEditForm() {
+let div = this.element.querySelector('.show-info')
+let venue = div.children[0].innerText.split(' - ')[0]
+let city = div.children[0].innerText.split(' - ')[1]
+let loadin = div.children[1].innerText.split('Load In Time: ')[1]
+let promoter = div.children[2].children[0].innerText.split('Promoter: ')[1].split(' - ')[0]
+let email = div.children[2].children[0].innerText.split('Promoter: ')[1].split(' - ')[1]
+let merch = div.children[3].children[0].innerText.split('Merch Total: $')[1]
+let guarantee = div.children[3].children[1].innerText.split('Show Guarantee: $')[1]
+let advanced = div.children[2].children[1].innerText.split('Show Advanced: ')[1]
+// debugger
+div.innerHTML = `
+<div class=edit-show-card>
+<form id= "edit-show-form">
+<h3>Edit Show</h3>
+<label for= "venue">Venue:</label>
+<input type="text" title="venue" id="show-venue" value= "${venue}">
+<label for="city">City:</label>
+<input type="text" title="city" id="show-city" placeholder="City, State" value= "${city}"></br>
+<label for="promoter">Promoter:</label>
+<input type="text" title="promoter" id="show-promoter" value= "${promoter}"></br>
+<label for="email">Promoter's Email:</label>
+<input type="text" title="email" id="show-email" pattern="^\S+@\S+$" value="${email}"></br>
+<label for="loadin">Load In Time and Date:</label>
+<input type="datetime-local" title="loadin" id="show-loadin" value="${loadin}"></br>
+<label for="advanced">Advanced?</label>
+<input type="checkbox" title="advanced" id="show-advanced" ${advanced === 'yes' ? 'checked' : 'unchecked'}></br>
+<label for="merch">Merch Total:</label>
+<input type="number" title="merch" id="show-merch" value = "${merch}"></br>
+<label for="guarantee">Show Guarantee:</label>
+<input type="number" title="guarantee" id="show-guarantee" value = "${guarantee}"></br>
+<input type="submit" value = "Create Show">
+</form>
+</div>`
+
+// const textInputArray = [...div.children[1].children.slice(0,-1)]
 }
 
 attachToDom() {
