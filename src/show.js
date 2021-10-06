f6d3734f91e9a9ef05ca8611c9de491762dedd75
Kyle Samuel <ksamuel92@gmail.com>
2021-10-06 12:46:00 -0400

    edit renderEditForm() for date. update handleClick

--- src/show.js
+++ src/show.js
@@ -22,9 +22,10 @@ constructor({advanced, date, email, guarantee, id, loadin, merch, promoter, user
 }
 
 render() {
+  // debugger
   this.element.innerHTML = `<div class="show-info">
   <h3>${this.venue} - ${this.city}</h3>
-  <h4> Load In Date and Time:${this.date} - ${Show.parseISOString(this.loadin)} </h4>
+  <h4> Load In Date and Time: ${this.date} - ${Show.parseISOString(this.loadin)} </h4>
   <ul>
   <li>Promoter: ${this.promoter} - ${this.email}</li>
   <li>Show Advanced: ${this.advanced ? 'yes' : 'no'}</li>
@@ -35,16 +36,21 @@ render() {
     <li class="show-totals">Total: $${this.merch + this.guarantee}</li>
   </ul>
   </div>
-  <button type="button" data-id:"${this.id}"}">Edit</button>
-  <button type="button" data-id:"${this.id}">Delete</button>
+  <button type="button" class="edit-btn" data-id:"${this.id}">Edit</button>
+  <button type="button" class="delete-btn" data-id:"${this.id}">Delete</button>
   `
   return this.element
 }
 
 handleClick = e => {
   if (e.target.innerText === 'Edit'){
+    e.target.innerText = 'Save'
     this.renderEditForm();
-  } else if (e.target.innerText === 'Delete'){
+  } else if (e.target.innerText === 'Save'){
+    e.target.innerText = 'Edit'
+    showCall.updateShow(e.target);
+  }
+  else if (e.target.innerText === 'Delete'){
   } else if (e.target.innerText === 'Save Comment') {
 
   }
@@ -54,39 +60,40 @@ renderEditForm() {
 let div = this.element.querySelector('.show-info')
 let venue = div.children[0].innerText.split(' - ')[0]
 let city = div.children[0].innerText.split(' - ')[1]
-let loadin = div.children[1].innerText.split('Load In Time: ')[1]
+let loadin = div.children[1].innerText.split(' - ')[1]
 let promoter = div.children[2].children[0].innerText.split('Promoter: ')[1].split(' - ')[0]
 let email = div.children[2].children[0].innerText.split('Promoter: ')[1].split(' - ')[1]
 let merch = div.children[3].children[0].innerText.split('Merch Total: $')[1]
-let date =
+let showDate = div.children[1].innerText.split(' - ')[0].split('Load In Date and Time: ')[1]
 let guarantee = div.children[3].children[1].innerText.split('Show Guarantee: $')[1]
 let advanced = div.children[2].children[1].innerText.split('Show Advanced: ')[1]
-debugger
+// debugger
 div.innerHTML = `
 <div class=edit-show-card>
 <form id= "edit-show-form">
 <h3>Edit Show</h3>
 <label for= "venue">Venue:</label>
-<input type="text" title="venue" id="show-venue" value= "${venue}">
+<input type="text" title="venue" id="edit-venue" value= "${venue}">
 <label for="city">City:</label>
-<input type="text" title="city" id="show-city" placeholder="City, State" value= "${city}"></br>
+<input type="text" title="city" id="edit-city" placeholder="City, State" value= "${city}"></br>
 <label for="promoter">Promoter:</label>
-<input type="text" title="promoter" id="show-promoter" value= "${promoter}"></br>
+<input type="text" title="promoter" id="edit-promoter" value= "${promoter}"></br>
 <label for="email">Promoter's Email:</label>
-<input type="text" title="email" id="show-email" pattern="^\S+@\S+$" value="${email}"></br>
-<label for="loadin">Load In Time and Date:</label>
-<input type="datetime-local" title="loadin" id="show-loadin" value="${Date.parse(loadin)}"></br>
+<input type="email" title="email" id="edit-email" value="${email}"></br>
+<label for="date">Show Date:</label>
+<input type="date" title="date" id="edit-date" value="${showDate}">
+<label for>Load In Time:</label>
+<input type="time" title="loadin" id="edit-loadin" value="${loadin}"><br>
 <label for="advanced">Advanced?</label>
-<input type="checkbox" title="advanced" id="show-advanced" ${advanced === 'yes' ? 'checked' : 'unchecked'}></br>
+<input type="checkbox" title="advanced" id="edit-advanced" ${advanced === 'yes' ? 'checked' : 'unchecked'}></br>
 <label for="merch">Merch Total:</label>
-<input type="number" title="merch" id="show-merch" value = "${merch}"></br>
+<input type="number" title="merch" id="edit-merch" value = "${merch}"></br>
 <label for="guarantee">Show Guarantee:</label>
-<input type="number" title="guarantee" id="show-guarantee" value = "${guarantee}"></br>
-<input type="submit" value = "Create Show">
+<input type="number" title="guarantee" id="edit-guarantee" value = "${guarantee}"></br>
 </form>
 </div>`
 
-// const textInputArray = [...div.children[1].children.slice(0,-1)]
+return this.element
 }
 
 attachToDom() {
@@ -102,8 +109,7 @@ static getGross() {
 
  static parseISOString(s) {
   var b = s.split(/\D+/);
-  debugger
-  return new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
+  return `${b[3]}:${b[4]}`
 }
 
 
