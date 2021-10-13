33b819d1ab5d998e09a1929d3cf1e0ef936a5b29
Kyle Samuel <ksamuel92@gmail.com>
2021-10-13 14:43:46 -0400

    refactored out variables to DomService

--- index.js
+++ index.js
@@ -2,52 +2,29 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 const port = 'http://localhost:3000';
-const userLoginForm = document.querySelector('#login-form');
-const userLoginFormDiv = document.querySelector('#login');
-const header = document.querySelector('#header');
-const headerDiv = document.querySelector('#header-section');
-const showInfo = document.querySelector('#shows-info');
-const newShowButton = document.querySelector('#new-show-button');
-const newShowFormDiv = document.querySelector('#new-show');
-const newShowFormCloseButton =
-  document.querySelector('#new-form-close');
 const showCall = new ShowService(port);
 const userCall = new UserService(port);
+const dom = new DomService();
 let loggedInUserEmail = null;
-const userNameValue = document.querySelector('#user-name');
-const userEmailValue = document.querySelector('#user-email');
-const userCard = document.querySelector('.user-card');
-const showContainer = document.querySelector('#show-container');
-const showGross = document.querySelector('#shows-gross');
-const newShowForm = document.querySelector('#new-show-form');
-const venueValue = document.querySelector('#show-venue');
-const cityValue = document.querySelector('#show-city');
-const promoterValue = document.querySelector('#show-promoter');
-const emailValue = document.querySelector('#show-email');
-const dateValue = document.querySelector('#show-date');
-const loadinValue = document.querySelector('#show-loadin');
-const merchValue = document.querySelector('#show-merch');
-const guaranteeValue = document.querySelector('#show-guarantee');
-const advancedValue = document.querySelector('#show-advanced');
 
 const handleUserLogin = (e) => {
   e.preventDefault();
-  userCall.createUser();
-  header.classList.add('header');
-  header.classList.remove('hidden');
-  showContainer.classList.remove('hidden');
-  showInfo.classList.remove('hidden');
-  showGross.classList.add('shows-gross');
-  showGross.classList.remove('hidden');
+  userCall.createUser(e.target);
+  dom.header.classList.add('header');
+  dom.header.classList.remove('hidden');
+  dom.showContainer.classList.remove('hidden');
+  dom.showInfo.classList.remove('hidden');
+  dom.showGross.classList.add('shows-gross');
+  dom.showGross.classList.remove('hidden');
   document.body.classList.remove('overlay');
-  userLoginFormDiv.classList.add('hidden');
+  dom.userLoginFormDiv.classList.add('hidden');
 };
 
 const handleSubmit = (e) => {
   e.preventDefault();
-  showCall.postShow();
-  newShowForm.reset();
-  newShowFormDiv.classList.add('hidden');
+  showCall.postShow(e.target);
+  dom.newShowForm.reset();
+  dom.newShowFormDiv.classList.add('hidden');
   document.body.classList.remove('overlay');
   document.querySelector('.user-card').style.backgroundColor =
     '#ffffff';
@@ -55,8 +32,8 @@ const handleSubmit = (e) => {
 
 const handleShowClose = (e) => {
   e.preventDefault();
-  newShowForm.reset();
-  newShowFormDiv.classList.add('hidden');
+  dom.newShowForm.reset();
+  dom.newShowFormDiv.classList.add('hidden');
   document.body.classList.remove('overlay');
   document.querySelector('.user-card').style.backgroundColor =
     '#ffffff';
@@ -65,13 +42,13 @@ const handleShowClose = (e) => {
 const handleKeyDown = (e) => {
   if (
     e.key === 'Escape' &&
-    !newShowFormDiv.classList.contains('hidden')
+    !dom.newShowFormDiv.classList.contains('hidden')
   ) {
     handleShowClose(e);
   }
 };
 
-userLoginForm.addEventListener('submit', handleUserLogin);
-newShowForm.addEventListener('submit', handleSubmit);
+dom.userLoginForm.addEventListener('submit', handleUserLogin);
+dom.newShowForm.addEventListener('submit', handleSubmit);
 document.addEventListener('keydown', handleKeyDown);
-newShowFormCloseButton.addEventListener('click', handleShowClose);
+dom.newShowFormCloseButton.addEventListener('click', handleShowClose);
