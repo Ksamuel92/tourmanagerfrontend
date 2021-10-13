b36b5b4e56e6bd05058eccb2b28e63d2fa9fdd67
Kyle Samuel <ksamuel92@gmail.com>
2021-10-13 14:43:57 -0400

    delete comments

--- src/dom_service.js
+++ src/dom_service.js
@@ -44,20 +44,3 @@ class DomService {
     return document.getElementById('new-show-form');
   }
 }
-
-// const userLoginForm = document.querySelector('#login-form');// TODO: add to DOM class.
-// const userLoginFormDiv = document.querySelector('#login');// TODO: add to DOM class.
-// const header = document.querySelector('#header'); // TODO: add to DOM class.
-// const headerDiv = document.querySelector('#header-section'); // TODO: add to DOM class.
-// const showInfo = document.querySelector('#shows-info'); // TODO: add to DOM class.
-// const newShowFormDiv = document.querySelector('#new-show'); // TODO: add to DOM class.
-// const newShowFormCloseButton =
-//   document.querySelector('#new-form-close'); //TODO: add to DOM class.
-// const showCall = new ShowService(port);
-// const userCall = new UserService(port);
-// let loggedInUserEmail = null;
-// const userCard = document.querySelector('.user-card'); // TODO: add to DOM class
-// const showContainer = document.querySelector('#show-container');// TODO: add to DOM class
-// const showGross = document.querySelector('#shows-gross'); // TODO: add to DOM class
-// const newShowForm = document.querySelector('#new-show-form'); //TODO: add to DOM class
-// //Too many variables!!!!!
