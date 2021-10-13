a23b839e47e44b65f61ca80a7979f7abbe3985b0
Kyle Samuel <ksamuel92@gmail.com>
2021-10-13 14:42:18 -0400

    add trash button, change innerText to classList

--- src/show.js
+++ src/show.js
@@ -59,74 +59,79 @@ class Show {
   <button type="button" class="delete-btn" data-id ="${
     this.id
   }">Delete</button>
+  <button type="button" class="trash-btn" data-id =${
+    this.id
+  }>Trash</button>
   </div>
   `;
     return this.element;
   }
 
+  // change innertext to class so devs can edit innerText
   handleClick = (e) => {
-    if (e.target.innerText === 'Edit') {
+    // debugger
+    if (e.target.classList.contains('edit-btn')) {
       e.target.innerText = 'Save';
-      this.renderEditForm();
-    } else if (e.target.innerText === 'Save') {
+      e.target.classList.remove('edit-btn');
+      e.target.classList.add('save-btn');
+      this.renderEditForm(this);
+    } else if (e.target.classList.contains('save-btn')) {
       e.target.innerText = 'Edit';
+      e.target.classList.remove('save-btn');
+      e.target.classList.add('edit-btn');
       this.updateShowInfo(e.target);
-    } else if (e.target.innerText === 'Delete') {
+    } else if (e.target.classList.contains('delete-btn')) {
       e.target.parentElement.parentElement.remove();
       Show.getGross();
-      showCall.deleteShow(e);
+      showCall.deleteShow(this);
+    } else if (e.target.classList.contains('trash-btn')) {
+      showCall.trashShow(this);
+      this.render();
     }
   };
 
   renderEditForm() {
     const div = this.element.querySelector('.show-info');
-    const venue = div.children[0].innerText.split(' - ')[0];
-    const city = div.children[0].innerText.split(' - ')[1];
-    const loadin = div.children[1].innerText.split(' - ')[1];
-    const promoter = div.children[2].children[0].innerText
-      .split('Promoter: ')[1]
-      .split(' - ')[0];
-    const email = div.children[2].children[0].innerText
-      .split('Promoter: ')[1]
-      .split(' - ')[1];
-    const merch =
-      div.children[3].children[0].innerText.split(
-        'Merch Total: $',
-      )[1];
-    const showDate = div.children[1].innerText
-      .split(' - ')[0]
-      .split('Load In Date and Time: ')[1];
-    const guarantee = div.children[3].children[1].innerText.split(
-      'Show Guarantee: $',
-    )[1];
-    const advanced =
-      div.children[2].children[1].innerText.split(
-        'Show Advanced: ',
-      )[1];
     div.innerHTML = `
 <div class=edit-show-card>
 <form data-set:"${this.id}"id= "edit-show-form">
 <h3>Edit Show</h3>
 <label for= "venue">Venue:</label>
-<input type="text" title="venue" id="edit-venue" value= "${venue}">
+<input type="text" title="venue" id="edit-venue" name= "venue" value= "${
+      this.venue
+    }">
 <label for="city">City:</label>
-<input type="text" title="city" id="edit-city" placeholder="City, State" value= "${city}"></br>
+<input type="text" title="city" id="edit-city" name = "city" placeholder="City, State" value= "${
+      this.city
+    }"></br>
 <label for="promoter">Promoter:</label>
-<input type="text" title="promoter" id="edit-promoter" value= "${promoter}"></br>
+<input type="text" title="promoter" id="edit-promoter" name="promoter" value= "${
+      this.promoter
+    }"></br>
 <label for="email">Promoter's Email:</label>
-<input type="email" title="email" id="edit-email" value="${email}"></br>
+<input type="email" title="email" id="edit-email" name="email" value="${
+      this.email
+    }"></br>
 <label for="date">Show Date:</label>
-<input type="date" title="date" id="edit-date" value="${showDate}">
+<input type="date" title="date" id="edit-date" name="date" value="${
+      this.date
+    }">
 <label for>Load In Time:</label>
-<input type="time" title="loadin" id="edit-loadin" value="${loadin}"><br>
-<input type="checkbox" title="advanced" id="edit-advanced" ${
-      advanced === 'yes' ? 'checked' : 'unchecked'
+<input type="time" title="loadin" id="edit-loadin" name="loadin" value="${
+      this.loadin
+    }"><br>
+<input type="checkbox" name="advanced" title="advanced" id="edit-advanced" ${
+      this.advanced === 'yes' ? 'checked' : 'unchecked'
     }>
 <label for="advanced">Advanced?</label></br>
 <label for="merch">Merch Total:</label>
-<input type="number" title="merch" id="edit-merch" value = "${merch}"></br>
+<input type="number" title="merch" id="edit-merch" name="merch" value = "${
+      this.merch
+    }"></br>
 <label for="guarantee">Show Guarantee:</label>
-<input type="number" title="guarantee" id="edit-guarantee" value = "${guarantee}"></br>
+<input type="number" title="guarantee" id="edit-guarantee" name="guarantee" value = "${
+      this.guarantee
+    }"></br>
 </form>
 </div>`;
 
@@ -134,39 +139,30 @@ class Show {
   }
 
   updateShowInfo(editBtn) {
-    const div = editBtn.parentNode.previousElementSibling;
-
-    const editVenueValue = div.querySelector('#edit-venue');
-    const editCityValue = div.querySelector('#edit-city');
-    const editPromoterValue = div.querySelector('#edit-promoter');
-    const editEmailValue = div.querySelector('#edit-email');
-    const editDateValue = div.querySelector('#edit-date');
-    const editLoadInValue = div.querySelector('#edit-loadin');
-    const splitTime = editLoadInValue.value.split(':');
-    const editAdvancedValue = div.querySelector('#edit-advanced');
-    const editMerchValue = div.querySelector('#edit-merch');
-    const editGuaranteeValue = div.querySelector('#edit-guarantee');
-    this.advanced = editAdvancedValue.value;
-    this.email = editEmailValue.value;
-    this.date = editDateValue.value;
-    this.guarantee = Number(editGuaranteeValue.value);
+    const form =
+      editBtn.parentNode.previousElementSibling.children[0]
+        .children[0];
+    this.advanced = form.advanced.value;
+    this.email = form.email.value;
+    this.date = form.date.value;
+    this.guarantee = Number(form.guarantee.value);
     this.loadin = new Date(
       1,
       1,
       2000,
-      splitTime[0] - 5,
-      splitTime[1],
+      form.loadin.value.split(':')[0],
+      form.loadin.value.split(':')[1],
     );
-    this.merch = Number(editMerchValue.value);
-    this.promoter = editPromoterValue.value;
-    this.venue = editVenueValue.value;
-    this.city = editCityValue.value;
+    this.merch = Number(form.merch.value);
+    this.promoter = form.promoter.value;
+    this.venue = form.venue.value;
+    this.city = form.city.value;
 
     showCall.updateShow(this);
   }
 
   attachToDom() {
-    showContainer.appendChild(this.element);
+    dom.showContainer.appendChild(this.element);
   }
 
   static getGross() {
@@ -174,7 +170,7 @@ class Show {
       document.querySelectorAll('.show-totals'),
     );
     if (showTotalsArray.length === 0) {
-      return (showGross.innerText = 'Total Gross: $0');
+      return dom.showGross.innerText = 'Total Gross: $0'; // return to avoid empty array on .reduce
     }
     const totalGross = showTotalsArray
       .map((show) =>
@@ -185,6 +181,6 @@ class Show {
       .reduce(
         (previousTotal, currentTotal) => previousTotal + currentTotal,
       );
-    return (showGross.innerText = `Total Gross: $${totalGross}`);
+    dom.showGross.innerText = `Total Gross: $${totalGross}`;
   }
 }
